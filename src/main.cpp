8718129aed750213b38544790c19317d8b4a193f
Geming400 <Geming400@gmail.com>
2025-05-25 19:22:51 +0200

    Added a desync check

--- src/main.cpp
+++ src/main.cpp
@@ -13,5 +13,13 @@ using namespace geode::prelude;
         if (Mod::get()->getSavedValue<bool>("first-time-loading", true)) {
             Mod::get()->setSavedValue<bool>("show-editor-button", false);
         }
+
+        if (Mod::get()->getSavedValue<bool>("show-editor-button", false) != Mod::get()->getSettingValue<bool>("show-editor-button")) {
+            const bool newValue = Mod::get()->getSavedValue<bool>("show-editor-button", false);
+
+            log::info("Desync found in setting 'first-time-loading'. Setting saved and setting value to {}", newValue);
+            Mod::get()->setSavedValue<bool>("show-editor-button", newValue);
+            Mod::get()->setSettingValue<bool>("show-editor-button", newValue)
+        }
     }
 #endif
