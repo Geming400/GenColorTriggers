06fa5bc231d3ff9de91c0e7ce9cc24e7801de7c0
Geming400 <Geming400@gmail.com>
2025-05-25 17:33:02 +0200

    Added a popup

--- src/hooks/EditorUI.cpp
+++ src/hooks/EditorUI.cpp
@@ -16,6 +16,11 @@ class $modify(MyEditorUI, EditorUI) {
 	bool init(LevelEditorLayer* editorLayer) {
 		if (!EditorUI::init(editorLayer)) return false;
 		
+        if (!Mod::get()->getSavedValue<bool>("first-time-loading", false)) { // if their are going into the editor for the first time after installing this then show a popup
+            Mod::get()->setSavedValue<bool>("first-time-loading", true);
+            FLAlertLayer::create("Hello!", "To generate color triggers please press 'F10' !", "Ok!")->show();
+        }
+
 		this->template addEventListener<InvokeBindFilter>([=](InvokeBindEvent* event) {
 			if (event->isDown()) {
 				m_fields->m_parsingNotification = Notification::create("Parsing level...", NotificationIcon::Loading, 5);
