32f0ed4c3c2820d60c642ffa7e241e4128fa9100
Geming400 <Geming400@gmail.com>
2025-06-07 00:14:02 +0200

    Added prettify triggers setting

--- src/hooks/LevelEditorLayer.cpp
+++ src/hooks/LevelEditorLayer.cpp
@@ -2,6 +2,7 @@
 #include "../utils/utils.hpp"
 #include "../parser/parser.hpp"
 #include "../utils/customColors.hpp"
+#include <cmath>
 
 
 #define COLOR_TRIGGER_OBJ_ID 899
@@ -34,67 +35,80 @@ bool isColorSet(const ccColor3B& color) {
 #define GET_COLOR_ACTION_DICT(levelSettings) levelSettings->m_effectManager->m_colorActionDict
 #define CCDICT_TO_CCDICTEXT(key, value, CCDict) 
 
-int MyLevelEditorLayer::genColorTriggers(GameObject* center, CCPoint offset) {	
-	CCArrayExt<EffectGameObject> objects;
 
-	auto editorUI = this->m_editorUI;
-
-	/*
-	for (const auto& [colorID, color] : CCDictionaryExt<int, ColorAction>(GET_COLOR_ACTION_DICT(m_levelSettings))) {
-		if (Mod::get()->getSettingValue<bool>("include-builtin-color-channels")) {
-			if (modUtils::isInVector(allowedCustomColors, colorID)) { continue; }
-		} else {
-			if (modUtils::isInVector(allCustomColors, colorID)) { continue; }
-		}
-
-		auto obj = static_cast<EffectGameObject*>(createObject(COLOR_TRIGGER_OBJ_ID, offset, false));
-
-		if (m_currentLayer != -1)
-			obj->m_editorLayer = m_currentLayer;
-		
-		if (isColorSet(color->m_color)) {
-			offset.y += 30;
-		}
-
-		objects.push_back(obj);
-	}*/
-
-	std::string levelString = getLevelString();
-
-	auto colorChannels = colorChannelsParser::getColorChannelsFromLevelString(levelString);
-
-	if (!colorChannels) {
-		Notification::create("No colour channels found!", NotificationIcon::Warning)->show();
-		return 0;
-	}
-	
-	bool pushingIntoVector = true;
-
-	for (const auto &colorTriggerContent : colorChannels.value()) {
+std::vector<modUtils::ColorTriggerContent> getGeneratableColorChannels(std::vector<modUtils::ColorTriggerContent> colorChannels) {
+	std::vector<modUtils::ColorTriggerContent> generatableColorChannels;
+	for (const auto &colorTriggerContent : colorChannels) {
 		if (Mod::get()->getSettingValue<bool>("include-builtin-color-channels")) {
 			if (!modUtils::isInVector(allowedCustomColors, colorTriggerContent.targetChannelID) && colorTriggerContent.targetChannelID >= 1000) { continue; }
 		} else {
 			if (colorTriggerContent.targetChannelID >= 1000) { continue; }
 		}
 
+		generatableColorChannels.push_back(colorTriggerContent);
+	}
+
+	return generatableColorChannels;
+}
+
+int MyLevelEditorLayer::genColorTriggers(GameObject* center, CCPoint offset) {	
+	const CCPoint originalOffset = offset;
+	const int64_t forEachTriggers = Mod::get()->getSettingValue<int64_t>("for-each-triggers");
+	const auto editorUI = this->m_editorUI;
+	bool pushingIntoVector = true;
+
+	std::string levelString = getLevelString();
+
+	auto colorChannels = colorChannelsParser::getColorChannelsFromLevelString(levelString);
+	if (!colorChannels) {
+		Notification::create("No colour channels found! (This isn't supposed to happen btw)", NotificationIcon::Error)->show();
+		log::error("No colour channels found! Please stop doing some tomfoolery to the gd editor or something, I beg :(");
+		return 0;
+	}
+
+	auto generatableColorChannels = getGeneratableColorChannels(colorChannels.value());
+	int colorChannelsNum = generatableColorChannels.size();
+
+	float prettifyXoffset;
+	int numColumns = ceil((double) colorChannelsNum / (double) forEachTriggers);
+	if (numColumns > 0) {
+		prettifyXoffset = 15 * (numColumns - 1);
+		log::debug("prettifyXoffset = {}", prettifyXoffset);
+
+		offset.x -= prettifyXoffset;
+	} else {
+		log::debug("numColumns < 0");
+	}
+
+
+	int i = 1;
+	CCArrayExt<EffectGameObject> objects;
+	for (const auto &colorTriggerContent : generatableColorChannels) {
 		auto obj = static_cast<EffectGameObject*>(createObject(COLOR_TRIGGER_OBJ_ID, offset, false));
 		
-		colorChannelsParser::colorTriggerContentToColorTrigger(obj, colorTriggerContent);
+		colorChannelsParser::colorTriggerContentToColorTrigger(obj, colorTriggerContent, Mod::get()->getSettingValue<bool>("use-legacy-hsv"));
 
 		if (m_currentLayer != -1) {
 			obj->m_editorLayer = m_currentLayer;
 		}
 		
-		offset.y += 30;
+		if (i % static_cast<int>(forEachTriggers) == 0 && Mod::get()->getSettingValue<bool>("prettify-trigger-pos")) {
+			offset.y = originalOffset.y;
+			offset.x += 30;
+		} else {
+			offset.y += 30;
+		}
 
 		if (pushingIntoVector) {
 			objects.push_back(obj);
 		}
 
-		if (objects.size() > vectorSizePushLimit) {
+		if (objects.size() > vectorSizePushLimit) { // Additional check just in case the user did some tomfoolery
 			pushingIntoVector = false;
-			log::warn("Exceeded 'objects' vector limit (= {})", vectorSizePushLimit);
+			log::warn("Exceeded 'objects' vector limit somehow, idfk what you did but ok (= {})", vectorSizePushLimit);
 		}
+
+		i++;
 	}
 	
 
