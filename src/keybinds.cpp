91daa24076441085c48c16a14381bd098c7d6c34
Geming400 <Geming400@gmail.com>
2025-07-22 22:45:59 +0200

    fixed silly indentation :3

--- src/keybinds.cpp
+++ src/keybinds.cpp
@@ -3,22 +3,22 @@
 using namespace geode::prelude;
 
 #ifdef GEODE_IS_DESKTOP
-    #include <geode.custom-keybinds/include/Keybinds.hpp>
-    using namespace keybinds;
+#include <geode.custom-keybinds/include/Keybinds.hpp>
+using namespace keybinds;
 
-    $execute {
-        BindManager::get()->registerBindable({
-            // ID, should be prefixed with mod ID
-            "genColorTriggers"_spr,
-            // Name
-            "Generate color triggers",
-            // Description, leave empty for none
-            "Generate color triggers for every channels",
-            // Default binds
-            { Keybind::create(KEY_F10, Modifier::None) },
-            // Category; use slashes for specifying subcategories. See the
-            // Category class for default categories
-            Category::EDITOR
-        });
-    }
+$execute {
+    BindManager::get()->registerBindable({
+        // ID, should be prefixed with mod ID
+        "genColorTriggers"_spr,
+        // Name
+        "Generate color triggers",
+        // Description, leave empty for none
+        "Generate color triggers for every channels",
+        // Default binds
+        { Keybind::create(KEY_F10, Modifier::None) },
+        // Category; use slashes for specifying subcategories. See the
+        // Category class for default categories
+        Category::EDITOR
+    });
+}
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,23 @@
 using namespace geode::prelude;
 
 #ifdef GEODE_IS_MOBILE
-    $on_mod(Loaded) {
-        Mod::get()->setSavedValue<bool>("show-editor-button", true);
-    }
+$on_mod(Loaded) {
+    Mod::get()->setSavedValue<bool>("show-editor-button", true);
+}
 #endif
 
 #ifdef GEODE_IS_DESKTOP
-    $on_mod(Loaded) {
-        if (Mod::get()->getSavedValue<bool>("first-time-loading", true)) {
-            Mod::get()->setSavedValue<bool>("show-editor-button", false);
-        }
-
-        if (Mod::get()->getSavedValue<bool>("show-editor-button", false) != Mod::get()->getSettingValue<bool>("show-editor-button")) {
-            const bool newValue = Mod::get()->getSettingValue<bool>("show-editor-button");
-
-            log::info("Desync found in setting 'first-time-loading'. Setting saved and setting value to {}", newValue);
-            Mod::get()->setSavedValue<bool>("show-editor-button", newValue);
-            Mod::get()->setSettingValue<bool>("show-editor-button", newValue);
-        }
+$on_mod(Loaded) {
+    if (Mod::get()->getSavedValue<bool>("first-time-loading", true)) {
+        Mod::get()->setSavedValue<bool>("show-editor-button", false);
     }
+
+    if (Mod::get()->getSavedValue<bool>("show-editor-button", false) != Mod::get()->getSettingValue<bool>("show-editor-button")) {
+        const bool newValue = Mod::get()->getSettingValue<bool>("show-editor-button");
+
+        log::info("Desync found in setting 'first-time-loading'. Setting saved and setting value to {}", newValue);
+        Mod::get()->setSavedValue<bool>("show-editor-button", newValue);
+        Mod::get()->setSettingValue<bool>("show-editor-button", newValue);
+    }
+}
 #endif
