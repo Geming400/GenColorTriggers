dcbd46ac737ea92a6b4bb6eae4dbce3dd5e4cd21
Geming400 <Geming400@gmail.com>
2025-10-03 23:50:02 +0200

    Force setting the `show-editor-button` saved value to `true` if CK isn't installed

--- src/settings.cpp
+++ src/settings.cpp
@@ -1,11 +1,19 @@
 #include <Geode/loader/SettingV3.hpp>
+#include "./utils/utils.hpp"
 
 using namespace geode::prelude;
 
 $execute {
     listenForSettingChanges("show-editor-button", [](bool value) {
+        // Note for the comment below, I didn't know that there was an argument for a default value
+        // in `Mod::getSettingValue<T>`
+        //
         // We store that setting as a saved value instead because this setting won't exist on mobile.
         // The value is initialiazed in 'main.cpp'
-        Mod::get()->setSavedValue<bool>("show-editor-button", value);
+        if (Loader::get()->isModLoaded(CUSTOM_KEYBINDS_MOD_ID)) {
+            Mod::get()->setSavedValue<bool>("show-editor-button", value);
+        } else {
+            Mod::get()->setSavedValue<bool>("show-editor-button", true);
+        }
     });
 }
