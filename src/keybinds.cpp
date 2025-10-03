880424263f3c4fb25baf73d0c3d9c64d20c82fef
Geming400 <Geming400@gmail.com>
2025-10-03 22:55:39 +0200

    forgot to remove the old keybind

--- src/keybinds.cpp
+++ src/keybinds.cpp
@@ -8,20 +8,20 @@ using namespace geode::prelude;
 using namespace keybinds;
 
 $execute {
-    BindManager::get()->registerBindable({
-        // ID, should be prefixed with mod ID
-        "genColorTriggers"_spr,
-        // Name
-        "Generate color triggers",
-        // Description, leave empty for none
-        "Generate color triggers for every channels",
-        // Default binds
-        { Keybind::create(KEY_F9, Modifier::None) },
-        // Category; use slashes for specifying subcategories. See the
-        // Category class for default categories
-        Category::EDITOR,
-        false // repeatable = false
-    });
+    // BindManager::get()->registerBindable({
+    //     // ID, should be prefixed with mod ID
+    //     "genColorTriggers"_spr,
+    //     // Name
+    //     "Generate color triggers",
+    //     // Description, leave empty for none
+    //     "Generate color triggers for every channels",
+    //     // Default binds
+    //     { Keybind::create(KEY_F9, Modifier::None) },
+    //     // Category; use slashes for specifying subcategories. See the
+    //     // Category class for default categories
+    //     Category::EDITOR,
+    //     false // repeatable = false
+    // });
 
     (void)[&]() -> Result<> {
         GEODE_UNWRAP(BindManagerV2::registerBindable(GEODE_UNWRAP(BindableActionV2::create(
